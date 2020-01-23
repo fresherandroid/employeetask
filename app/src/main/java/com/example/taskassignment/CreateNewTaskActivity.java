cae6391922a79cc3930e7a29f52bd0110f5cbb4e
fresher124 <fresher.and.mob.app@gmail.com>
2020-01-23 11:48:53 +0530

    Task Creation

--- app/src/main/java/com/example/taskassignment/CreateNewTaskActivity.java
+++ app/src/main/java/com/example/taskassignment/CreateNewTaskActivity.java
@@ -8,24 +8,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CreateNewTaskActivity extends AppCompatActivity {
 
     EditText createNewTask;
     Button save;
+    private FirebaseUser currentUser;
+    private DatabaseReference myRef;
+    int taskId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_task);
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        myRef = FirebaseDatabase.getInstance().getReference();
+
         createNewTask = findViewById(R.id.create_new_task);
         save = findViewById(R.id.button_save_task);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                createTaskInFirebase(createNewTask.getText().toString());
 //                finish();
 //                Intent intent = new Intent(CreateNewTaskActivity.this, MainActivity.class);
 //                startActivity(intent);
             }
         });
+    }
+
+    public void createTaskInFirebase(String taskCreated) {
+        Task task = new Task(taskCreated, currentUser.getUid());
+        myRef.child("tasks").child(String.valueOf(taskId)).setValue(task);
+        taskId++;
     }
 }
--- app/src/main/java/com/example/taskassignment/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/taskassignment/ui/home/HomeViewModel.java
@@ -1,16 +1,46 @@
 package com.example.taskassignment.ui.home;
 
+import android.content.Intent;
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.taskassignment.EmployeeActivity;
+import com.example.taskassignment.ManagerActivity;
+import com.example.taskassignment.SignInActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private FirebaseAuth mAuth;
+    private DatabaseReference myRef;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mAuth = FirebaseAuth.getInstance();
+        myRef = FirebaseDatabase.getInstance().getReference("tasks");
+        myRef.child(String.valueOf(0)).child("task").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String task = dataSnapshot.getValue(String.class);
+                mText.setValue(task);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
     }
 
     public LiveData<String> getText() {
